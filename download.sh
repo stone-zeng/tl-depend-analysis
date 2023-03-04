82e24cd19cf1a746799950037b3c9a152f30c9c6
Xiangdong Zeng <xdzeng96@gmail.com>
2023-03-04 23:40:12 +0800

    refactor `main.py`; fix github actions

--- download.sh
+++ download.sh
@@ -1,4 +1,10 @@
-MIRROR=https://mirrors.sjtug.sjtu.edu.cn/ctan
+if [ -z "$1" ]; then
+    MIRROR=https://mirror.ctan.org
+else
+    IFS="="
+    read -ra ADDR <<< "$1"
+    MIRROR=${ADDR[1]}
+fi
 
 curl -o texlive.tlpdb.xz $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.xz
 xz -d -f texlive.tlpdb.xz
@@ -6,3 +12,5 @@ xz -d -f texlive.tlpdb.xz
 curl -o texlive.tlpdb.sha512 $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.sha512
 sha512sum -c texlive.tlpdb.sha512
 rm texlive.tlpdb.sha512
+
+mv texlive.tlpdb data/
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import dataclasses
 import json
 import os
 import subprocess
 import sys
+from typing import Any
 
 from file_parser import Parser
 
 
-TLPDB_PATH = 'tlpdb/texlive.tlpdb'
-TL_PACKAGES_PATH = 'data/tl-packages.json'
-TL_FILES_PATH = 'data/tl-files-nofonts.csv'
+TLPDB_PATH = 'data/texlive.tlpdb'
 TL_DEPENDENCIES_PATH = 'data/tl-dependencies.json'
 
 TEXMFDIST_PATH = subprocess.run(
@@ -17,104 +17,117 @@ TEXMFDIST_PATH = subprocess.run(
 # TEXMFDIST_PATH = '/usr/local/texlive/2023/texmf-dist'
 
 
-def get_packages():
-    with open(TLPDB_PATH, 'r', encoding='utf-8') as fp:
-        items = fp.read().strip().split('\n\n')
+@dataclasses.dataclass
+class Package:
+    name: str
+    category: str
+    revision: int
+    tl_depend: list[str]
+    depend: list[str]
+    runfiles: list[str]
 
-    packages: list[dict[str, str | list[str]]] = []
-    for item in items:
-        lines = item.split('\n')
-        _, name = lines[0].split()
-        if not name.startswith('00') and '.' not in name:
-            packages.append({'name': name} | _parse_item(lines))
 
-    with open(TL_PACKAGES_PATH, 'w', encoding='utf-8') as fp:
-        json.dump(packages, fp, indent=2)
+class PackageEncoder(json.JSONEncoder):
 
-    files: list[str] = []
-    for item in packages:
-        name = item['name']
-        if name.endswith('-dev'):  # type: ignore
-            print('Skip dev package: ', name, file=sys.stderr)
-            continue
-        for file in item.get('runfiles', []):
-            try:
-                prefix, path = file.split('/', maxsplit=1)
-            except ValueError:
+    def default(self, o):
+        if isinstance(o, Package):
+            return {
+                'name': o.name,
+                'depend': o.depend,
+                'tl_depend': o.tl_depend,
+            }
+            # return dataclasses.asdict(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class TLDependencies:
+
+    def __init__(self):
+        self.packages: list[Package] = []
+        self.file_mappings: dict[str, str] = {}
+
+    def parse_tlpdb(self):
+        with open(TLPDB_PATH, 'r', encoding='utf-8') as fp:
+            items = fp.read().strip().split('\n\n')
+        for item in items:
+            lines = item.split('\n')
+            _, name = lines[0].split()
+            if not name.startswith('00') and '.' not in name:
+                self.packages.append(Package(name=name, depend=[], **self._parse_tlpdb_item(lines)))
+
+    @staticmethod
+    def _parse_tlpdb_item(lines: list[str]):
+        package: dict[str, Any] = {
+            'tl_depend': [],
+            'runfiles': [],
+        }
+        runfiles_flag = False
+        for line in lines:
+            key, *value = line.strip().split(maxsplit=1)
+            value = value[0] if value else None
+            match key:
+                case 'category':
+                    package['category'] = value
+                case 'revision':
+                    package['revision'] = int(value) if value else -1
+                case 'depend':
+                    package['tl_depend'].append(value)
+                case 'runfiles':
+                    runfiles_flag = True
+                case _ if runfiles_flag:
+                    if line.startswith(' '):
+                        package['runfiles'].append(line.strip())
+                    else:
+                        runfiles_flag = False
+        return package
+
+    def get_file_mappings(self):
+        for package in self.packages:
+            if package.name.endswith('-dev'):
+                print('Skip dev package:', package.name, file=sys.stderr)
                 continue
-            if prefix in ('RELOC', 'texmf-dist') and not path.startswith('fonts'):
-                files.append(f'{os.path.basename(path)},{name}\n')
-    files.sort()
+            for file in package.runfiles:
+                if file.startswith('RELOC') or file.startswith('texmf-dist'):
+                    _, path = file.split('/', maxsplit=1)
+                    if path.startswith('fonts'):
+                        continue
+                    if (name := os.path.basename(path)) in self.file_mappings:
+                        print('Duplicate file:', file, file=sys.stderr)
+                    else:
+                        self.file_mappings[name] = package.name
 
-    with open(TL_FILES_PATH, 'w', encoding='utf-8') as fp:
-        fp.writelines(files)
+    def get_dependencies(self):
+        for package in self.packages:
+            depend: set[str] = set()
+            for file in package.runfiles:
+                depend.update(self._get_dependencies_from_file(file))
+            package.depend = sorted(depend)
 
-
-def _parse_item(lines: list[str]):
-    res = {}
-    depend = []
-    runfiles = []
-    runfiles_flag = False
-    for line in lines:
-        if line.startswith('category'):
-            res['category'] = line.removeprefix('category ')
-        elif line.startswith('revision'):
-            res['revision'] = int(line.removeprefix('revision '))
-        elif line.startswith('shortdesc'):
-            res['description'] = line.removeprefix('shortdesc ')
-        elif line.startswith('depend'):
-            depend.append(line.removeprefix('depend '))
-
-        if depend:
-            res['depend'] = depend
-
-        if line.startswith('runfiles'):
-            runfiles_flag = True
-        elif runfiles_flag:
-            if line.startswith(' '):
-                runfiles.append(line.strip())
-            else:
-                runfiles_flag = False
-
-        if runfiles:
-            res['runfiles'] = runfiles
-
-    return res
+    def _get_dependencies_from_file(self, file: str):
+        depend: set[str] = set()
+        if file.startswith('RELOC') or file.startswith('texmf-dist'):
+            _, path = file.split('/', maxsplit=1)
+            if path.startswith('fonts'):
+                return depend
+            fullpath = os.path.join(TEXMFDIST_PATH, path)
+            parser = Parser(fullpath)
+            parser.parse()
+            for d in parser.dep:
+                try:
+                    depend.add(self.file_mappings[d])
+                except KeyError:
+                    print('Dependency not found:', d, file=sys.stderr)
+        return depend
 
 
 def main():
-    with open(TL_PACKAGES_PATH, 'r', encoding='utf-8') as fp:
-        packages: list[dict] = json.load(fp)
-
-    with open(TL_FILES_PATH, 'r', encoding='utf-8') as fp:
-        files_mapping = {}
-        for line in fp.readlines():
-            name, pkg = line.split(',')
-            files_mapping[name] = pkg.strip()
-
-    dependencies = []
-    for package in packages:
-        depend = set()
-        for runfile in package.get('runfiles', []):
-            try:
-                prefix, path = runfile.split('/', maxsplit=1)
-            except ValueError:
-                continue
-            if prefix in ('RELOC', 'texmf-dist') and not path.startswith('fonts'):
-                fullpath = os.path.join(TEXMFDIST_PATH, path)
-                parser = Parser(fullpath)
-                parser.parse()
-                if parser.dep:
-                    depend.update(files_mapping[d] for d in parser.dep if d in files_mapping)
-        dependencies.append({
-            'name': package['name'],
-            'tl-depend': package.get('depend', []),
-            'depend': sorted(depend),
-        })
-        with open(TL_DEPENDENCIES_PATH, 'w', encoding='utf-8') as fp:
-            json.dump(dependencies, fp, indent=2)
+    analyzer = TLDependencies()
+    analyzer.parse_tlpdb()
+    analyzer.get_file_mappings()
+    analyzer.get_dependencies()
+    with open(TL_DEPENDENCIES_PATH, 'w', encoding='utf-8') as fp:
+        json.dump(analyzer.packages, fp, cls=PackageEncoder, indent=2)
 
 
 if __name__ == '__main__':
-    # get_packages()
     main()
