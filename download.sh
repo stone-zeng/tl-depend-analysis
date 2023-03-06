4aaed442d2ae618ee02a0bd03d1010f84be74552
Xiangdong Zeng <xdzeng96@gmail.com>
2023-03-06 23:21:58 +0800

    avoid using xz

--- download.sh
+++ download.sh
@@ -4,9 +4,7 @@ else
     MIRROR=$1
 fi
 
-curl -o texlive.tlpdb.xz $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.xz
-xz -d -f texlive.tlpdb.xz
-
+curl -o texlive.tlpdb        $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb
 curl -o texlive.tlpdb.sha512 $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.sha512
 sha512sum -c texlive.tlpdb.sha512
 rm texlive.tlpdb.sha512
