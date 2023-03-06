e7eb9d71ae179c2f6c807ea055f03a1ff21a2743
Xiangdong Zeng <xdzeng96@gmail.com>
2023-03-07 00:05:06 +0800

    Use container (#1)
    * build: use container
    * build: simplify `download.sh` args
    * avoid using xz
    * build: revert to rit mirror
    * build: config `safe.directory` [debug]
    * build: use `pwd` [debug]
    * build: toggle comments

--- download.sh
+++ download.sh
@@ -1,14 +1,10 @@
 if [ -z "$1" ]; then
     MIRROR=https://mirror.ctan.org
 else
-    IFS="="
-    read -ra ADDR <<< "$1"
-    MIRROR=${ADDR[1]}
+    MIRROR=$1
 fi
 
-curl -o texlive.tlpdb.xz $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.xz
-xz -d -f texlive.tlpdb.xz
-
+curl -o texlive.tlpdb        $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb
 curl -o texlive.tlpdb.sha512 $MIRROR/systems/texlive/tlnet/tlpkg/texlive.tlpdb.sha512
 sha512sum -c texlive.tlpdb.sha512
 rm texlive.tlpdb.sha512
