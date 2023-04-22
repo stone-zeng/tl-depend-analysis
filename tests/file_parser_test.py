7de10033bb59f2728acbc162757271cc7f87c4dd
Xiangdong Zeng <xdzeng96@gmail.com>
2023-04-22 23:15:10 +0800

    add tests

---
+++ tests/file_parser_test.py
@@ -0,0 +1,30 @@
+# ruff: noqa: E501
+
+import os
+import unittest
+
+from file_parser import Parser
+
+
+class Test(unittest.TestCase):
+    def test_tex_basic(self):
+        expect = {'article.cls', 'geometry.sty', 'amsmath.sty', 'amsfonts.sty', 'fontspec.sty', 'hyperref.sty', 'expl3.sty', 'natbib.sty'}
+        self._test('basic.tex', expect)
+
+    def test_tex_comments(self):
+        expect = {'beamer.cls', 'geometry.sty', 'tikz.sty', 'tikz-cd.sty', 'tabularx.sty', 'tabulary.sty', 'natbib.sty'}
+        self._test('comments.tex', expect)
+
+    def test_lua(self):
+        expect = {'module-1.lua', 'module-7.lua', 'module-8.lua'}
+        self._test('basic.lua', expect)
+
+    def _test(self, file: str, expect: set[str]):
+        path = os.path.join(os.path.dirname(__file__), 'test-data', file)
+        parser = Parser(path)
+        parser.parse()
+        self.assertSetEqual(parser.depend, expect)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test-data/basic.lua
@@ -0,0 +1,19 @@
+require("module-1")
+-- require("module-2")
+
+--[[
+    require("module-3")
+]]
+
+---[[ require("module-4") ]]
+
+--[[
+    require("module-5")
+--]]
+
+--[[doc--
+    require("module-6")
+--doc]]--
+
+loadmodule("module-7.lua")
+loadmodule "module-8.lua"
